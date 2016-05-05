28b4553329206ba15248caac91d7e304ed82c734
Josealejandrograjalesp <alejogrpa@outlook.com>
2016-05-05 12:31:23 -0500

    trabajo

---
+++ html/html/codigo.js
@@ -0,0 +1,11 @@
+
+var nombre = prompt('cual es tu nombre?');
+
+if(nombre == 'alejandro') 
+{
+	alert('Bienvenido: '+nombre+' el aprendiz');
+}
+else
+{
+	alert('Bienvenido: '+nombre+' el mejor');
+}
